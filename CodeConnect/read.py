a2f6c9b6e9bf3f0b3e8702e542cf2891c33ea074
FoxNerdSaysMoo <zceboys@gmail.com>
2020-10-09 15:34:04 +0000

    Bug Fixes

--- CodeConnect/read.py
+++ CodeConnect/read.py
@@ -16,21 +16,24 @@ def get_code(Name, Id, read_file):
             # If the line is a header, find out the name
             if line[0] == '[':
                 curr_name = line[1:line.find(']')]
+                trimmed_lines.append(line)
                 continue
                 
             # If it is the correct header, try to get the id
             elif curr_name == Name and len(line.split()) >= 1:
                 if line.split()[0] == 'id':
-                    curr_id = line.split()[2:]
+                    curr_id = line.split()[2]
             
             if curr_name == Name and curr_id == Id:
                 if line.rstrip():
                     trimmed_lines.append(line.rstrip())
                     success = True
         if not success:
-        	raise ItemNotFoundError(f'Item {Name} with Id "{Id}", was not found')
+        	raise ItemNotFoundError(f'Item "{Name}" with Id "{Id}", was not found')
     
+    print(trimmed_lines)
     code = Code(Name, trimmed_lines[1].split()[2], trimmed_lines[2].split()[2], [], [], [code[1:] for code in trimmed_lines[7:]])
     return code
 
-get_code("test1", '000000 test1', 'default.codeconnect')
+if __name__ == '__main__':
+    get_code("test1", '000000', 'CodeConnect/default.codeconnect')
